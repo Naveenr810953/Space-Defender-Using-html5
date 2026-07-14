c7b2047d6456a5469c75ee31d3d40dbaf64ff8f5
Naveen Kumar S <156082282+Naveenr810953@users.noreply.github.com>
2026-07-14 22:12:17 +0530

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Game variables 
+// Game variables  
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');  
 const scoreElement = document.getElementById('score');   
